6d4c45b21a55285b2f9ed63cbee1f0c5ac6eafb4
Mr.Igor <iv_mirsoft@ukr.net>
2025-05-09 17:29:40 +0300

    hw-6

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,33 @@
-function getShippingMessage(country, price, deliveryFee) {
+class Storage {
+	#items;
+	constructor(itemsArr) {
+		this.#items = itemsArr;
+	}
 
-	const totalPrice = price + deliveryFee;
-	return `Shipping to ${country} will cost ${totalPrice} credits`;
+	getItems() {
+		return this.#items;
+	}
+
+	addItem(itemName) {
+		this.#items.push(itemName);
+	}
+
+	removeItem(itemName) {
+		const indexDel = this.#items.indexOf(itemName);
+		indexDel >= 0 ? this.#items.splice(indexDel, 1) : console.log(`Item: ${itemName} is not in Storage`);
+	}
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,32 @@
+class StringBuilder {
+	#value = "";
+	constructor(value) {
+		this.#value = value;
+	}
 
-function getElementWidth(content, padding, border) {
-	const contentNumber = Number.parseFloat(content);
-	const paddingNumber = Number.parseFloat(padding);
-	const borderNumber = Number.parseFloat(border);
+	getValue() {
+		return this.#value;
+	}
 
-	let totalWidth = contentNumber + 2 * paddingNumber + 2 * borderNumber;
+	padStart(value) {
+		this.#value = value + this.#value;
+	}
 
-	return totalWidth;
+	padEnd(value) {
+		this.#value = this.#value + value;
+	}
 
+	padBoth(value) {
+		this.#value = value + this.#value + value;
+	}
 }
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
